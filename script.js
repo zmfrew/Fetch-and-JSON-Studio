50e6b8979c6ef0ffa08ef37da81709ca10f1ed00
Zach Frew <zachary.frew@ascension-external.org>
2020-03-19 20:44:31 -0500

    Add last name to astronauts

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
 // TODO: add code here
+
+function init() {
+    getAstronauts()
+}
+
+function getAstronauts() {
+    fetch('https://handlers.education.launchcode.org/static/astronauts.json').then((response) => {
+        response.json().then((json) => {
+            const container = document.getElementById('container')
+            const sorted = json.sort((a, b) => b.hoursInSpace - a.hoursInSpace)
+            container.innerHTML += `<h2>Found ${sorted.length} astronauts</h2>`
+            for (let astronaut of sorted) {
+                const color = astronaut.active ? 'green' : 'black'
+                container.innerHTML += `
+                    <div class="astronaut">
+                        <div class="bio">
+                            <h3>${astronaut.firstName} ${astronaut.lastName}</h3>
+                            <ul>
+                                <li>Hours in space: ${astronaut.hoursInSpace}</li>
+                                <li style="color:${color}">Active: ${astronaut.active}</li>
+                                <li>Skills: ${astronaut.skills}</li>
+                            </ul>
+                        </div>
+                        <img class="avatar" src="${astronaut.picture}">
+                    </div>
+                `
+            }
+        })
+    })
+}
+
+window.onload = init
